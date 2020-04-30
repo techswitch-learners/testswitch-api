055580d30ef7e388e297ad37fb10f9593ece709a
espe-on <eljoe0@googlemail.com>
2020-04-30 11:13:15 +0100

    Added SessionID Data Migration

---
+++ TestSwitchApi/TestSwitchApi/DataMigrations/005_AddAdminUserAndSessionIDTables.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE public."AdminUsers" (
+                                     "Id" BIGINT NOT NULL PRIMARY KEY UNIQUE,
+                                     "Email" VARCHAR(256) NOT NULL UNIQUE,
+                                     "HashedPassword" VARCHAR(128) NOT NULL,
+                                     "PasswordSalt" VARCHAR(24) NOT NULL
+);
+
+CREATE TABLE public."AdminSessionID"(
+                                        "Id" BIGINT NOT NULL PRIMARY KEY UNIQUE,
+                                        "AdminUserID" BIGINT NOT NULL,
+                                        "SessionStart" TIMESTAMP NOT NULL,
+                                        "SessionEnd" TIMESTAMP NOT NULL
+);
+
+ALTER TABLE ONLY public."AdminSessionID"
+    ADD CONSTRAINT AdminUserID_fk FOREIGN KEY ("AdminUserID") REFERENCES public."AdminUsers"("Id") NOT VALID;
