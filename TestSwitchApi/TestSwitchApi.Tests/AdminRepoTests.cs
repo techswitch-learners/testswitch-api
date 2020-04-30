3b6c396c0cf9d39f67dc2159a80af2c48f7f38e5
Jamie Baird <james.baird@softwire.com>
2020-04-30 14:58:23 +0100

    Added a test class for admin repository

---
+++ TestSwitchApi/TestSwitchApi.Tests/AdminRepoTests.cs
@@ -0,0 +1,24 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+using TestSwitchApi.Repositories;
+
+namespace TestSwitchApi.Tests
+{
+    public class AdminRepoTests
+    {
+        // private AdminRepo _adminRepo;
+        //
+        // [SetUp]
+        // public void SetUp()
+        // {
+        //     _adminRepo = new AdminRepo(_context);
+        // }
+        //
+        //
+        // [Test]
+        // public CheckIsLoginPasswordValid()
+        // {
+        //     
+        // }
+    }
+}
