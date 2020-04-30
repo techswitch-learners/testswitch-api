a0e3100148a3c30247154e014278ed49db6ff3af
espe-on <eljoe0@googlemail.com>
2020-04-30 12:30:04 +0100

    Data model for pulling Admin Users from DB

---
+++ TestSwitchApi/TestSwitchApi/Models/DataModels/AdminUserDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestSwitchApi.Models.DataModels
+{
+    public class AdminUserDataModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string PasswordSalt { get; set; }
+
+    }
+}
