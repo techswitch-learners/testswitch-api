cf29809b166b7d0baa21b09d9d67fb8b4b9c3d80
Jamie Baird <james.baird@softwire.com>
2020-04-30 14:57:50 +0100

    Added some endpoints to controller,
    -One tests GetAdmin method
    -One will be our eventual endpoint

--- TestSwitchApi/TestSwitchApi/Controllers/AdminSigninController.cs
+++ TestSwitchApi/TestSwitchApi/Controllers/AdminSigninController.cs
@@ -1,10 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestSwitchApi.Models.DataModels;
+using TestSwitchApi.Repositories;
 
 namespace TestSwitchApi.Controllers
 {
     [ApiController]
     [Route("/sign-in")]
-    public class AdminSigninController
+    public class AdminSigninController : Controller
     {
+        private readonly IAdminRepo _adminRepo;
+        public AdminSigninController(IAdminRepo adminRepo)
+        {
+            _adminRepo = adminRepo;
+        }
+
+        [HttpPost("")]
+        public ActionResult<string> AttemptLogin([FromForm] string email, string password)
+        {
+            //test if exists in DB
+            //--if not then return 403 response
+            var adminUser = _adminRepo.GetAdminByEmail(email);
+            if (adminUser == null)
+            {
+                return Unauthorized();
+            }
+
+            var passwordValid = _adminRepo.IsLoginPasswordValid(password, adminUser.PasswordSalt, adminUser.HashedPassword);
+            var sessionId = " ";
+            return sessionId;
+        }
+
+        [HttpPost("test")]
+        public AdminUserDataModel CheckEmailExists([FromForm] string email)
+        {
+            return _adminRepo.GetAdminByEmail(email);
+        }
     }
 }
