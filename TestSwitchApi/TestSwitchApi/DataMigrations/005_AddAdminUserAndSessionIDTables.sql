2c76a5715bfcbb18483a5037bd8108dfad2a78dd
espe-on <eljoe0@googlemail.com>
2020-04-30 11:52:49 +0100

    added auto increment to IDs

--- TestSwitchApi/TestSwitchApi/DataMigrations/005_AddAdminUserAndSessionIDTables.sql
+++ TestSwitchApi/TestSwitchApi/DataMigrations/005_AddAdminUserAndSessionIDTables.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE public."AdminUsers" (
-                                     "Id" BIGINT NOT NULL PRIMARY KEY UNIQUE,
+                                     "Id" BIGSERIAL NOT NULL PRIMARY KEY UNIQUE,
                                      "Email" VARCHAR(256) NOT NULL UNIQUE,
                                      "HashedPassword" VARCHAR(128) NOT NULL,
                                      "PasswordSalt" VARCHAR(24) NOT NULL
 );
 
 CREATE TABLE public."AdminSessionID"(
-                                        "Id" BIGINT NOT NULL PRIMARY KEY UNIQUE,
+                                        "Id" BIGSERIAL NOT NULL PRIMARY KEY UNIQUE,
                                         "AdminUserID" BIGINT NOT NULL,
                                         "SessionStart" TIMESTAMP NOT NULL,
                                         "SessionEnd" TIMESTAMP NOT NULL
