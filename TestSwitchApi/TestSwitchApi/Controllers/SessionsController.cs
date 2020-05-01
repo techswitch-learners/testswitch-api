a13eabd58b120741e517b4804d05d7ecc4ddc2a3
hnalabanda <hemanalabanda@gmail.com>
2020-05-01 15:08:36 +0100

    changes so that a candidate can submit a test

--- TestSwitchApi/TestSwitchApi/Controllers/SessionsController.cs
+++ TestSwitchApi/TestSwitchApi/Controllers/SessionsController.cs
@@ -21,7 +21,7 @@ namespace TestSwitchApi.Controllers
         }
 
         [HttpPost("{tokenId}")]
-        public ActionResult<CandidateTestModel> AddTestSubmission([FromRoute] string tokenId, [FromForm] AddTestSubmissionRequestModel newSubmission)
+        public ActionResult<CandidateTestModel> AddTestSubmission([FromRoute] string tokenId, [FromBody] AddTestSubmissionRequestModel newSubmission)
         {
             if (!ModelState.IsValid)
             {
@@ -31,7 +31,15 @@ namespace TestSwitchApi.Controllers
             var candidate = _candidates.GetCandidateByGuid(tokenId);
             if (candidate!=null)
             {
-                return _submissions.AddTestSubmission(candidate.Id, newSubmission);
+                try
+                {
+                    _submissions.AddTestSubmission(candidate.Id, newSubmission);
+                    return StatusCode(200);
+                }
+                catch
+                {
+                    return StatusCode(500);
+                }
             }
 
             return StatusCode(401, "Invalid token");
