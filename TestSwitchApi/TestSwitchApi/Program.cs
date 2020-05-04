1a8665a3b6fb2aa0ab605f2208d6a5d916b74f66
espe-on <eljoe0@googlemail.com>
2020-05-04 12:45:40 +0100

    Program on startup will create Admin user if admin user doesn't already exist

--- TestSwitchApi/TestSwitchApi/Program.cs
+++ TestSwitchApi/TestSwitchApi/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TestSwitchApi.Models.ApiModels;
+using TestSwitchApi.Models.DataModels;
+using TestSwitchApi.Services;
 
 namespace TestSwitchApi
 {
@@ -13,11 +15,40 @@ namespace TestSwitchApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+           CreateAdminUsersIfNotExist(host);
+           host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        private static void CreateAdminUsersIfNotExist(IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<TestSwitchDbContext>();
+            context.Database.EnsureCreated();
+            if (!context.AdminUsers.Any())
+            {
+                var passwordService = new PasswordService();
+                string newAdminEmail = Environment.GetEnvironmentVariable("DEFAULTADMINEMAIL");
+                string newAdminPassword = Environment.GetEnvironmentVariable("DEFAULTADMINPASSWORD");
+                byte[] newAdminPasswordSalt = passwordService.GenerateSalt();
+                string hashedPassword =
+                    passwordService.HashPassword(
+                        newAdminPassword,
+                        newAdminPasswordSalt);
+                var newAdminUser = new AdminUserDataModel()
+                {
+                    Email = newAdminEmail,
+                    PasswordSalt = newAdminPasswordSalt,
+                    HashedPassword = hashedPassword,
+                };
+                context.AdminUsers.Add(newAdminUser);
+                context.SaveChanges();
+            }
+        }
     }
 }
